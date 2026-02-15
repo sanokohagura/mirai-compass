ba047e234d9f4de8869567abda094b9f3aae71af
sanokohagura <sanokohagura@gmail.com>
2026-02-15 15:37:26 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,17 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    // process.env を含めてロードするよう修正
-    const env = loadEnv(mode, process.cwd(), '');
-    
-    return {
-      // GitHub Pagesのディレクトリ構造に合わせる
-      base: './', 
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        // 重要：GitHub Secretsから渡されたキーを、ビルド時にプログラムに書き込みます
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY || process.env.GEMINI_API_KEY),
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY || process.env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+export default defineConfig({
+    base: './', 
+    plugins: [react()],
+    define: {
+      // process.env 経由での呼び出しにも対応させます
+      'process.env.GEMINI_API_KEY': JSON.stringify(process.env.VITE_GEMINI_API_KEY),
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    }
 });
